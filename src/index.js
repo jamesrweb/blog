3c749a57261397993f304c2d8e45805615ec0b66
jamesrweb <jamesrweb@users.noreply.github.com>
2021-06-25 13:53:33 +0000

    Apply formatting changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 const { Elm } = require('./Main.elm');
 
 Elm.Main.init({
-	node: document.getElementById('app'),
-	flags: process.env.FOREM_API_KEY
+  node: document.getElementById('app'),
+  flags: process.env.FOREM_API_KEY
 });
--- webpack.config.js
+++ webpack.config.js
@@ -10,49 +10,49 @@ const outputDirectory = resolve(__dirname, 'dist');
 const isProduction = ENV === 'production';
 
 const elmLoader = {
-	loader: 'elm-webpack-loader',
-	options: {
-		debug: false,
-		optimize: isProduction,
-		cwd: __dirname
-	}
+  loader: 'elm-webpack-loader',
+  options: {
+    debug: false,
+    optimize: isProduction,
+    cwd: __dirname
+  }
 };
 
 const loaders = isProduction
-	? elmLoader
-	: [{ loader: 'elm-hot-webpack-loader' }, elmLoader];
+  ? elmLoader
+  : [{ loader: 'elm-hot-webpack-loader' }, elmLoader];
 
 module.exports = {
-	mode: isProduction ? 'production' : 'development',
-	entry: './src/index.js',
-	devServer: {
-		publicPath: '/',
-		contentBase: outputDirectory,
-		port: 8000,
-		hotOnly: true
-	},
-	output: {
-		publicPath: '/',
-		path: outputDirectory,
-		filename: '[name].[contenthash].bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.elm$/,
-				exclude: [/elm-stuff/, /node_modules/],
-				use: loaders
-			}
-		]
-	},
-	plugins: [
-		new webpack.NoEmitOnErrorsPlugin(),
-		new HtmlWebpackPlugin({
-			author: 'James Robb',
-			title: 'James Robb | Blog'
-		}),
-		new webpack.DefinePlugin({
-			'process.env': JSON.stringify(env.parsed)
-		})
-	]
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/index.js',
+  devServer: {
+    publicPath: '/',
+    contentBase: outputDirectory,
+    port: 8000,
+    hotOnly: true
+  },
+  output: {
+    publicPath: '/',
+    path: outputDirectory,
+    filename: '[name].[contenthash].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: loaders
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      author: 'James Robb',
+      title: 'James Robb | Blog'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(env.parsed)
+    })
+  ]
 };
